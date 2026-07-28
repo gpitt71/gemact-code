3f9501802b74fcca10f81281d1133a982f8dffed
Gabriele Pittarello <pwt887@ku.dk>
2026-07-28 15:09:25 +0200

    reversioning

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "1.1.1"
+version = "1.3.0"
 short_description = "A comprehensive actuarial package for non-life (re)insurance."
 
 def readme():
